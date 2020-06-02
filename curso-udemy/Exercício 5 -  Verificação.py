a68244da78b5bb31cdb5bd543d63ab286d292440
Tiago Lima <tiaguinho.alima@gmail.com>
2020-06-02 16:43:45 -0300

    Destaca com cor os números dividos por 11 e com resto 5 entre 1000 e 2000

---
+++ curso-udemy/Exercício 5 -  Verificação.py
@@ -0,0 +1,11 @@
+color = {'red': '\033[41m', 'green': '\033[42m', 'close': '\033[m'}
+ncolor = {'red': '\033[31m', 'green': '\033[32m', 'close': '\033[m'}
+
+print(f'Legenda'.center(20))
+print(f'Número com resto 5:     {color["green"]}     {color["close"]}\n'
+      f'Número com outro resto: {color["red"]}     {color["close"]}')
+for number in range(1000, 2001):
+    if number % 11 == 5:
+        print(f'{ncolor["green"]}{number}{ncolor["close"]}')
+    else:
+        print(f'{ncolor["red"]}{number}{ncolor["close"]}')
