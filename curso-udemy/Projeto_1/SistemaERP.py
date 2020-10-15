a9d68ec818db782b03a9aca71349fb25dada8301
Tiago Lima <tiaguinho.alima@gmail.com>
2020-10-15 13:38:08 -0300

    Implementando as funções de login e sigup
    Signed-off-by: Tiago Lima <tiaguinho.alima@gmail.com>

--- curso-udemy/Projeto_1/SistemaERP.py
+++ curso-udemy/Projeto_1/SistemaERP.py
@@ -1,12 +1,47 @@
 import pymysql.cursors
 import pymysql as sql
 
-conecxao = sql.connect(
-    host='localhost',
-    port=3308,
-    user='root',
-    password='4321.',
-    db='erp',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
+
+def connection():
+    con = sql.connect(
+        host='localhost',
+        port=3308,
+        user='root',
+        password='4321.',
+        db='erp',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    return con
+
+
+def login():
+    pass
+
+
+def signup():
+    pass
+
+
+auth = False
+connect = connection()
+
+while not auth:
+    while True:
+        decision = int(input('[1] Login\n[2] Novo cadastro\n-> '))
+        if decision in (1, 2):
+            break
+
+    try:
+        with connect.cursor() as cursor:
+            cursor.execute('select * from cadastros')
+            usersfound = cursor.fetchall()
+            print(usersfound)
+    except:
+        print('Erro ao se comunicar com o banco de dados')
+    else:
+        if decision:
+            login()
+        else:
+            signup()
